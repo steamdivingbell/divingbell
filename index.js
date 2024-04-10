3325a004befcedca93e700dcffe2a65266bbfb90
jbzdarkid <jbzdarkid@gmail.com>
2024-04-09 22:09:06 -0700

    Seems to be working, mostly

--- index.js
+++ index.js
@@ -18,26 +18,39 @@ function set(id, key, value) {
   var elem = document.getElementById(id)
   if (key == 'innerText') {
     elem.innerText = value
+  } else if (key == 'style') {
+    elem.style = value
   } else {
     elem.setAttribute(key, value)
   }
 }
 
-function setImageCard(loc, gameId) {
-  if (games[gameId]) set(loc + '-title', 'innerText', games[gameId]['name'])
+var styles = {
+  'Hidden Gem': 'background: blue; color: white; font-weight: bold',
+  'Similar tags': 'background: green; color: white; font-weight: bold',
+  'Reverse match': 'background: purple; color: white; font-weight: bold',
+  'Loose match': 'background: brown; color: white; font-weight: bold',
+  'Selected': 'background: white; color: black; font-weight: bold',
+}
+
+function setImageCard(loc, gameId, recommender, note) {
+  set(loc + '-cell', 'style', styles[recommender])
+  set(loc + '-title', 'innerText', recommender)
   set(loc + '-title', 'href', 'https://store.steampowered.com/app/' + gameId)
+  if (note != null) set(loc + '-note', 'innerText', note)
   set(loc + '-image', 'src', 'https://cdn.akamai.steamstatic.com/steam/apps/' + gameId + '/header.jpg')
 }
 
-    
 function loadImages() {
-  setImageCard('tl', '670750')
-  setImageCard('tm', '243220')
-  setImageCard('tr', '512790')
-  setImageCard('ml', '866440')
-  setImageCard('mm', '210970')
-  setImageCard('mr', '258520')
-  setImageCard('bl', '383870')
-  setImageCard('bm', '251110')
-  setImageCard('br', '624270')
+  setImageCard('tl', '670750', 'Hidden Gem', '62%')
+  setImageCard('tm', '243220', 'Hidden Gem', '62%')
+  setImageCard('tr', '512790', 'Similar tags', '80%')
+  setImageCard('mr', '258520', 'Similar tags', '65%')
+  setImageCard('ml', '866440', 'Reverse match')
+  setImageCard('bl', '383870', 'Reverse match')
+  setImageCard('bm', '251110', 'Loose match')
+  setImageCard('br', '624270', 'Loose match')
+  setImageCard('mm', '210970', 'Selected')
+  
+  set('game-title', 'innerText', games['210970']['name'])
 }
